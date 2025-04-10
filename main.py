61fe7e71e35dfce89b81aa0c421bc34ef1d82c5f
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2025-04-10 09:32:49 -0400

    fixed the number mod, and sprites

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from text import Text
 from sprite import Sprite
 
 
+
 class Window():
     def __init__(self, width, height):
         self.width = width
@@ -24,18 +25,26 @@ def main():
     clock = pygame.time.Clock()
     window = Window(800, 600)
 
+
     number = 1
     upgrade_one = 0
 
-    number_mod = number + upgrade_one
 
+    number_mod = 1 + upgrade_one
+
+        # upgrade button worth
     UO_checknumber = 10
+
     sprite_width = 64
 
     sheet_info = [(i * sprite_width, 0) for i in range(10)]
 
 
+
     while True:
+
+
+
         """ put everything under this line """
         window.clear()
 
@@ -44,13 +53,16 @@ def main():
         upgrade_one_button = Buttons((0, 255, 0), 600, 100, 50, 50)
 
         """ put all text here """
-        main_text = Text((10, 10), str(number), "arial", 24)
+        main_text = Text((10, 10), str(number), "arial", 36)
+        upgrade_one_text = Text((600, 50), str(upgrade_one), "arial", 36)
 
         """ put sprites here """
         main_sprite = Sprite((200, 100), r"Numbers\number_sprite.png", (255,0,0), (200, 50),sheet_info=sheet_info)
+        button_sprite = Sprite((350, 250), r"data\sprites\button.png", (255,0,0), (64,64),sheet_info=sheet_info)
         # main_sprite = Sprite((200, 100), r"Numbers\number_sprite.png", (255,0,0), (200, 50),sheet_info=sheet_info)
 
         get_mouse_pos = pygame.mouse.get_pos()
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -58,11 +70,18 @@ def main():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if main_button.rect.collidepoint(get_mouse_pos):
-                    number += number_mod
-                    print(f" number total {number}")
+                    number = number + number_mod
+                    print(f"number mod {number_mod}")
+                    print(f"upgrade {upgrade_one}")
+                    print(f"total {number}")
                 if number >= UO_checknumber and upgrade_one_button.rect.collidepoint(get_mouse_pos):
                     upgrade_one += 1
-                    print(f"upgrade {upgrade_one}")
+                    number_mod = upgrade_one + 1
+                    if number > 50:
+                        UO_checknumber += 10
+                    elif number > 100:
+                        UO_checknumber += 50
+
 
         """ button widgets here """
         main_button.draw(window.screen)
@@ -70,20 +89,24 @@ def main():
 
         """ text widgets here"""
         main_text.render(window.screen)
+        upgrade_one_text.render(window.screen)
+
+        """ sprite widgets here """
 
         """ rendered sprites here """
 
         
         def sprite_render():
-            index = 0
+            index = number
+            (x,y) = 100,10
             for index, (x,y) in enumerate(main_sprite.sheet_info):
-                if index == upgrade_one:
-                    main_sprite.position = (x, y)
-                    main_sprite.render(window.screen, index)
-                    break
+                main_sprite.position = (x, y)
+                main_sprite.render(window.screen, index)
+                break
 
+        # sprite_render()
 
-        sprite_render()
+        button_sprite.render(window.screen, 0)
 
 
         clock.tick(60)
