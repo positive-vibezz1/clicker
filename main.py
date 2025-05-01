f8d86862c7f6f640c7e87609bad7e5c35a51179f
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2025-05-01 11:43:56 -0400

    added oil drop as upgrade button

--- main.py
+++ main.py
@@ -43,22 +43,22 @@ def main():
 
     while True:
 
-
-
         """ put everything under this line """
         window.clear()
 
-        """ put all rect widgets here """
+        """ put rect here """
         main_button = Buttons((255, 0, 0), 350, 250, 50, 50)
         upgrade_one_button = Buttons((0, 255, 0), 600, 100, 50, 50)
 
-        """ put all text here """
+        """ put text here """
         main_text = Text((10, 10), str(number), "arial", 36)
         upgrade_one_text = Text((600, 50), str(upgrade_one), "arial", 36)
+        total_click_amount = Text((10, 50), str(f"you have {number_mod} oil per click"), "arial", 36)
 
         """ put sprites here """
-        main_sprite = Sprite((200, 100), r"Numbers\number_sprite.png", (255,0,0), (200, 50),sheet_info=sheet_info)
+        # main_sprite = Sprite((200, 100), r"Numbers\number_sprite.png", (255,0,0), (200, 50),sheet_info=sheet_info)
         button_sprite = Sprite((350, 250), r"data\sprites\oil_rig_inverted.png", (255,0,0), size=(128,128),sheet_info=sheet_info)
+        upgrade_one_button_sprite = Sprite((625, 65), r"data\sprites\oil_outline.png", (255,0,0), size=(64,64),sheet_info=sheet_info)
         # main_sprite = Sprite((200, 100), r"Numbers\number_sprite.png", (255,0,0), (200, 50),sheet_info=sheet_info)
 
         get_mouse_pos = pygame.mouse.get_pos()
@@ -74,7 +74,8 @@ def main():
                     print(f"number mod {number_mod}")
                     print(f"upgrade {upgrade_one}")
                     print(f"total {number}")
-                if number >= UO_checknumber and upgrade_one_button.rect.collidepoint(get_mouse_pos):
+                    print(f"mouse pos {get_mouse_pos}")
+                if number >= UO_checknumber and upgrade_one_button_sprite.rect.collidepoint(get_mouse_pos):
                     upgrade_one += 1
                     number_mod = upgrade_one + 1
                     if number > 50:
@@ -84,29 +85,23 @@ def main():
 
 
         """ button widgets here """
-        main_button.draw(window.screen)
-        upgrade_one_button.draw(window.screen)
+        #main_button.draw(window.screen)
+        #upgrade_one_button.draw(window.screen)
 
         """ text widgets here"""
         main_text.render(window.screen)
         upgrade_one_text.render(window.screen)
+        total_click_amount.render(window.screen)
+
 
         """ sprite widgets here """
 
         """ rendered sprites here """
 
-        
-        def sprite_render():
-            index = number
-            (x,y) = 100,10
-            for index, (x,y) in enumerate(main_sprite.sheet_info):
-                main_sprite.position = (x, y)
-                main_sprite.render(window.screen, index)
-                break
-
         # sprite_render()
 
         button_sprite.render(window.screen, 0)
+        upgrade_one_button_sprite.render(window.screen, 0)
 
 
         clock.tick(60)
